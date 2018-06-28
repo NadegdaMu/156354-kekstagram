acd2955cdfc39f7af16ebbdc203725f320ceecd3
nadegdaMu <bykovanadegda@gmail.com>
2018-06-28 19:56:02 +0700

    Исправлены синтаксические ошибки

--- js/interaction-form.js
+++ js/interaction-form.js
@@ -1,28 +1,28 @@
 'use strict';
 // взаимодействие с формой редактирования фотографии
 (function () {
-var ESC_KEYCODE = 27;
+  var ESC_KEYCODE = 27;
+  var uploadImage = document.querySelector('.img-upload__overlay'); // поле редактирования фотографии
+  var uploadFile = document.querySelector('#upload-file'); // при наступлении события в поле загрузки файла, форма редоктирования фотографии становится доступным
 
-var uploadImage = document.querySelector('.img-upload__overlay'); // поле редактирования фотографии
-var uploadFile = document.querySelector('#upload-file'); // при наступлении события в поле загрузки файла, форма редоктирования фотографии становится доступным
-uploadFile.addEventListener('change', function () {
-  uploadImage.classList.remove('hidden');
-});
+  uploadFile.addEventListener('change', function () {
+    uploadImage.classList.remove('hidden');
+  });
 
-// при клике на крестике поле убирается
-var closeImageForm = document.querySelector('#upload-cancel');
-closeImageForm.addEventListener('click', function () {
-  uploadImage.classList.add('hidden');
-  uploadFile.value = '';
-});
+  // при клике на крестике поле убирается
+  var closeImageForm = document.querySelector('#upload-cancel');
+  closeImageForm.addEventListener('click', function () {
+    uploadImage.classList.add('hidden');
+    uploadFile.value = '';
+  });
 
-// при нажатии кнопки ESC поле так же закрывается
-document.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === ESC_KEYCODE) {
-    if (evt.target.className !== 'text__hashtags' || evt.target.className !== 'text__description') {
-      uploadImage.classList.add('hidden');
-      uploadFile.value = '';
+  // при нажатии кнопки ESC поле так же закрывается
+  document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      if (evt.target.className !== 'text__hashtags' || evt.target.className !== 'text__description') {
+        uploadImage.classList.add('hidden');
+        uploadFile.value = '';
+      }
     }
-  }
-});
+  });
 })();
--- js/pictures.js
+++ js/pictures.js
@@ -1,94 +1,95 @@
 'use strict';
+(function () {
+  var comments = [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  ];
 
-var comments = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
+  var description = [
+    'Тестим новую камеру!',
+    'Затусили с друзьями на море',
+    'Как же круто тут кормят',
+    'Отдыхаем...',
+    'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
+    'Вот это тачка!'
+  ];
 
-var description = [
-  'Тестим новую камеру!',
-  'Затусили с друзьями на море',
-  'Как же круто тут кормят',
-  'Отдыхаем...',
-  'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
-  'Вот это тачка!'
-];
+  // функция возвращающая случайный елемент массива
+  var getRandomItem = function (array) {
+    var randomIndex = array[Math.floor(Math.random() * (array.length))];
+    return randomIndex;
+  };
 
-// функция возвращающая случайный елемент массива
-var getRandomItem = function (array) {
-  var randomIndex = array[Math.floor(Math.random() * (array.length))];
-  return randomIndex;
-};
-
-// функция генерирующая целое число в диапазоне, включая минимальное и максимальное.
-var getRandomInRange = function (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+  // функция генерирующая целое число в диапазоне, включая минимальное и максимальное.
+  var getRandomInRange = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
 
 
-var userPhotos = [];
+  var userPhotos = [];
 
-// функция, генерирующая userPhotos - массив фотографий пользователей
-var generateUserPhotos = function () {
-  for (var i = 0; i < 25; i++) {
-    userPhotos[i] = {};
-    userPhotos[i].url = 'photos/' + (i + 1) + '.jpg';
-    userPhotos[i].likes = getRandomInRange(15, 200);
-    userPhotos[i].comments = getRandomItem(comments);
-    userPhotos[i].description = getRandomItem(description);
-  }
-};
-var sameUserPhotoElement = document.querySelector('.pictures');
-var sameUserPhotoTemplate = document.querySelector('#picture').content.querySelector('.picture__link');
+  // функция, генерирующая userPhotos - массив фотографий пользователей
+  var generateUserPhotos = function () {
+    for (var i = 0; i < 25; i++) {
+      userPhotos[i] = {};
+      userPhotos[i].url = 'photos/' + (i + 1) + '.jpg';
+      userPhotos[i].likes = getRandomInRange(15, 200);
+      userPhotos[i].comments = getRandomItem(comments);
+      userPhotos[i].description = getRandomItem(description);
+    }
+  };
+  var sameUserPhotoElement = document.querySelector('.pictures');
+  var sameUserPhotoTemplate = document.querySelector('#picture').content.querySelector('.picture__link');
 
-// функция отрисовывающая фотографии
-var renderUserPhotos = function (photo) {
-  var userPhotoElement = sameUserPhotoTemplate.cloneNode(true);
-  userPhotoElement.querySelector('.picture__img').src = photo.url;
-  userPhotoElement.querySelector('.picture__stat--likes').textContent = photo.likes;
-  userPhotoElement.querySelector('.picture__stat--comments').textContent = photo.comments;
-  return userPhotoElement;
-};
+  // функция отрисовывающая фотографии
+  var renderUserPhotos = function (photo) {
+    var userPhotoElement = sameUserPhotoTemplate.cloneNode(true);
+    userPhotoElement.querySelector('.picture__img').src = photo.url;
+    userPhotoElement.querySelector('.picture__stat--likes').textContent = photo.likes;
+    userPhotoElement.querySelector('.picture__stat--comments').textContent = photo.comments;
+    return userPhotoElement;
+  };
 
-// вызов заполнения массива объектов данных для фотографий
-generateUserPhotos();
+  // вызов заполнения массива объектов данных для фотографий
+  generateUserPhotos();
 
-var fragment = document.createDocumentFragment();
-userPhotos.forEach(function (element) {
-  fragment.appendChild(renderUserPhotos(element)); // вызов отрисовки фотографий
-});
-/* for (var i = 0; i < userPhotos.length; i++) {
-  fragment.appendChild(renderUserPhotos(userPhotos[i]));
-}; */
-sameUserPhotoElement.appendChild(fragment);
+  var fragment = document.createDocumentFragment();
+  userPhotos.forEach(function (element) {
+    fragment.appendChild(renderUserPhotos(element)); // вызов отрисовки фотографий
+  });
+  /* for (var i = 0; i < userPhotos.length; i++) {
+    fragment.appendChild(renderUserPhotos(userPhotos[i]));
+  }; */
+  sameUserPhotoElement.appendChild(fragment);
 
-var bigPicture = document.querySelector('.big-picture');
-// bigPicture.classList.remove('hidden'); делает большую фотографию видимой
+  var bigPicture = document.querySelector('.big-picture');
+  // bigPicture.classList.remove('hidden'); делает большую фотографию видимой
 
-// заполнение большой фотографии данными
-bigPicture.querySelector('.big-picture__img').src = userPhotos[0].url;
-bigPicture.querySelector('.likes-count').textContent = userPhotos[0].likes;
-bigPicture.querySelector('.comments-count').textContent = userPhotos[0].comments;
+  // заполнение данными большой фотографии данными
+  bigPicture.querySelector('.big-picture__img').src = userPhotos[0].url;
+  bigPicture.querySelector('.likes-count').textContent = userPhotos[0].likes;
+  bigPicture.querySelector('.comments-count').textContent = userPhotos[0].comments;
 
-var listComments = document.querySelector('.social__comments');
-var comment = document.createDocumentFragment();
-var newElement = document.createElement('li');
-newElement.className = 'social__comment social__comment--text';
-var newElementImg = document.createElement('img');
-newElementImg.src = 'img/avatar-' + getRandomInRange(1, 6) + '.svg';
-newElementImg.alt = 'Аватар комментатора фотографии';
-newElementImg.height = 35;
-newElementImg.width = 35;
-newElement.appendChild(newElementImg);
-// newElement.innerHTML = '<img class="social__picture" src="img/avatar-' + getRandomInRange(15, 200) + '.svg" alt="Аватар комментатора фотографии" width="35" height="35">';
-newElement.insertAdjacentText = ('beforeend', 'текст комментария');
-comment.appendChild(newElement);
-listComments.appendChild(comment);
+  var listComments = document.querySelector('.social__comments');
+  var comment = document.createDocumentFragment();
+  var newElement = document.createElement('li');
+  newElement.className = 'social__comment social__comment--text';
+  var newElementImg = document.createElement('img');
+  newElementImg.src = 'img/avatar-' + getRandomInRange(1, 6) + '.svg';
+  newElementImg.alt = 'Аватар комментатора фотографии';
+  newElementImg.height = 35;
+  newElementImg.width = 35;
+  newElement.appendChild(newElementImg);
+  // newElement.innerHTML = '<img class="social__picture" src="img/avatar-' + getRandomInRange(15, 200) + '.svg" alt="Аватар комментатора фотографии" width="35" height="35">';
+  newElement.insertAdjacentText = ('beforeend', 'текст комментария');
+  comment.appendChild(newElement);
+  listComments.appendChild(comment);
 
-bigPicture.querySelector('.social__caption').textContent = userPhotos[0].description;
-document.querySelector('.social__comment-count').classList.add('visually-hidden');
-document.querySelector('.social__loadmore').classList.add('visually-hidden');
+  bigPicture.querySelector('.social__caption').textContent = userPhotos[0].description;
+  document.querySelector('.social__comment-count').classList.add('visually-hidden');
+  document.querySelector('.social__loadmore').classList.add('visually-hidden');
+})();
