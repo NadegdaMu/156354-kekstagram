9376ed6c408a4e47d6002b885c2ccd014320b8ec
nadegdaMu <bykovanadegda@gmail.com>
2018-06-28 19:36:39 +0700

    Разделение кода на модули

---
+++ js/filter.js
@@ -0,0 +1,112 @@
+'use strict';
+(function () {
+// при выборе эффекта к основной фотографии применяется класс соответствующего эффекта
+window.imagePreview = document.querySelector('.img-upload__preview');
+var baseClassImagePreview = imagePreview.classList.item(0);
+
+var generateEffects = function (element, effectname, defaultfilter) {
+  var effect = document.querySelector('#effect-' + effectname);
+  effect.addEventListener('click', function () {
+    if (effect === 'none') {
+      element.classList = [];
+      element.style.filter = '';
+      element.classList.add(baseClassImagePreview);
+      element.classList.add('effects__preview--' + effectname);
+      document.querySelector('.scale__line').classList.add('visually-hidden');
+    } else {
+      element.classList = [];
+      element.style.filter = '';
+      element.classList.add(baseClassImagePreview);
+      element.classList.add('effects__preview--' + effectname);
+      sliderElement.style.left = '100%';
+      sliderLevel.style.width = '100%';
+      element.style.filter = defaultfilter;
+    }
+  });
+  return true;
+};
+
+var effectsList = ['none', 'chrome', 'sepia', 'marvin', 'phobos', 'heat'];
+var effectsDefStyle = ['', 'grayscale(1)', 'sepia(1)', 'invert(100)', 'blur(3)', 'brightness(3)'];
+
+for (var i = 0; i < effectsList.length; i++) {
+  generateEffects(imagePreview, effectsList[i], effectsDefStyle[i]);
+}
+
+// функция интенсивности эффекта
+var step = null;
+var effectMethodsList =
+{
+  'effects__preview--chrome': 1,
+  'effects__preview--sepia': 1,
+  'effects__preview--marvin': 100,
+  'effects__preview--phobos': 3,
+  'effects__preview--heat': 3
+};
+
+var generatesDepthEffect = function (sliderLevel) {
+  for (var key in effectMethodsList) {
+    if (key === imagePreview.classList[1]) {
+      applyingMethodsEffect(key, sliderLevel);
+      step = effectMethodsList[key] * sliderLevel;
+      break;
+    }
+  }
+};
+
+// функция генерирующая глубину эффекта
+var applyingMethodsEffect = function (filterName) {
+
+  if (filterName === ('effects__preview--chrome')) {
+    window.imagePreview.style.filter = 'grayscale(' + step + ')';
+  } else if (filterName === ('effects__preview--sepia')) {
+    window.imagePreview.style.filter = 'sepia(' + step + ')';
+  } else if (filterName === ('effects__preview--marvin')) {
+    window.imagePreview.style.filter = 'invert(' + step + '%)';
+  } else if (filterName === ('effects__preview--phobos')) {
+    window.imagePreview.style.filter = 'blur(' + step + 'px)';
+  } else if (filterName === ('effects__preview--heat')) {
+    window.imagePreview.style.filter = 'brightness(' + step + ')';
+  }
+};
+
+
+// реализация ползунка эффектов
+var startSlideX = 400;
+var sliderElement = document.querySelector('.scale__pin');
+var sliderLine = document.querySelector('.scale__line');
+var sliderLevel = document.querySelector('.scale__level');
+var sliderValue = document.querySelector('.scale__value');
+
+sliderLine.addEventListener('mousedown', startSlide, false);
+sliderLine.addEventListener('mouseup', stopSlide, false);
+
+function startSlide(event) {
+  var setPerc = ((((event.clientX - startSlideX) / sliderLine.offsetWidth)));
+  sliderLine.addEventListener('mousemove', moveSlide, false);
+  sliderElement.style.left = (setPerc * 100) + '%';
+  sliderLevel.style.width = (setPerc * 100) + '%';
+  sliderValue.value = parseInt(setPerc * 100, 10);
+  generatesDepthEffect(setPerc);
+}
+
+function moveSlide(event) {
+  var setPerc = ((((event.clientX - startSlideX) / sliderLine.offsetWidth)));
+  if (setPerc > 1 || setPerc < 0) {
+    stopSlide(event);
+  }
+  sliderElement.style.left = (setPerc * 100) + '%';
+  sliderLevel.style.width = (setPerc * 100) + '%';
+  sliderValue.value = parseInt(setPerc * 100, 10);
+  generatesDepthEffect(setPerc);
+}
+
+function stopSlide(event) {
+  var setPerc = ((((event.clientX - startSlideX) / sliderLine.offsetWidth)));
+  sliderLine.removeEventListener('mousemove', moveSlide, false);
+  sliderElement.style.left = (setPerc * 100) + '%';
+  sliderLevel.style.width = (setPerc * 100) + '%';
+  sliderValue.value = parseInt(setPerc * 100, 10);
+  generatesDepthEffect(setPerc);
+}
+})();
---
+++ js/form.js
@@ -0,0 +1,78 @@
+'use strict';
+(function () {
+// валидация хеш-тегов
+var hashtags = document.querySelector('.text__hashtags');
+
+var validationHashtag = function (hashtagElement) {
+
+  if (!hashtagElement.startsWith('#')) {
+    hashtags.setCustomValidity('Сообщение должно начинаться со знака #');
+    return false;
+  } else if (hashtagElement.lastIndexOf('#') !== 0) {
+    hashtags.setCustomValidity('нет пробела');
+    return false;
+  } else {
+    hashtags.setCustomValidity('');
+  }
+
+  if (hashtagElement.length < 2 || hashtagElement.length > 21) {
+    hashtags.setCustomValidity('Сообщение должно содержать больше одного и меньше 20 букв');
+    return false;
+  } else {
+    hashtags.setCustomValidity('');
+    return true;
+  }
+};
+
+var validationHashtagsArray = function (array) {
+  if (array.length > 5) {
+    hashtags.setCustomValidity('Сообщение не может содержать больше 5 записей');
+    return false;
+  } else {
+    hashtags.setCustomValidity('');
+  }
+
+  var element = '';
+  var sameArray = [];
+  var test = false;
+  for (var l = 0; l < array.length; l++) {
+    sameArray[l] = array[l].toLowerCase();
+  }
+  while (sameArray.length) {
+    element = sameArray.shift();
+    if (sameArray.includes(element) === true) {
+      hashtags.setCustomValidity('Все заметки должны быть разными');
+      test = false;
+      break;
+    } else {
+      test = true;
+      hashtags.setCustomValidity('');
+    }
+  }
+  return test;
+};
+
+
+// валидация формы
+var form = document.querySelector('.img-upload__form');
+
+hashtags.addEventListener('input', function () {
+  var hashtagstring = hashtags.value;
+  var hashtagsArray = hashtagstring.split(' ');
+  if (validationHashtagsArray(hashtagsArray)) {
+    for (var s = 0; s < hashtagsArray.length; s++) {
+      validationHashtag(hashtagsArray[s]);
+    }
+  }
+});
+
+
+form.addEventListener('submit', function (e) {
+  if (!form.checkValidity()) {
+    e.preventDefault();
+    return false;
+  }
+
+  return true;
+});
+})();
