ae76c0bd6d84ff385d91d352526f093e7d6711ef
nadegdaMu <bykovanadegda@gmail.com>
2018-07-11 11:54:31 +0700

    Перенесены css свойства в css файл

--- js/pictures.js
+++ js/pictures.js
@@ -47,9 +47,7 @@
 
   var errorHandler = function (errorMessage) {
     var node = document.createElement('div');
-    node.style = 'z-index: 100; width: 600px; height: 50px; padding-top: 15px; margin-top: 300px; margin-left: 335px; text-align: center; background-color: #6cdc09;';
-    node.style.position = 'absolute';
-    node.style.fontSize = '30px';
+    node.classList.add('error__message');
     node.textContent = errorMessage;
     document.body.insertAdjacentElement('afterbegin', node);
   };
--- js/utils.js
+++ js/utils.js
@@ -24,9 +24,6 @@
     generateMessage: function () {
       var node = document.createElement('div');
       node.classList.add('modal__message');
-      node.style = 'z-index: 100; width: 600px; height: 50px; padding-top: 15px; margin-top: 300px; margin-left: 335px; text-align: center; background-color: #dcd009;';
-      node.style.position = 'fixed';
-      node.style.fontSize = '26px';
       node.textContent = 'Данные успешно отправлены';
       document.body.insertAdjacentElement('afterbegin', node);
     }
